6f225f63609261683ae619cb236d1ccfd8ea2232
Alex Ding <alexding123@outlook.com>
2020-12-07 23:39:28 -0500

    Added corner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import GithubCorner from "react-github-corner";
 import GamePage from "./pages/GamePage";
 
 function App() {
   return (
     <div className="App">
+      <GithubCorner href="https://github.com/alexander-ding/24/"/>
       <GamePage/>
     </div>
   );
